d1ab5072c1e02a500c7dbd06c50334264561ff54
Himani Soni <sonihimani428@gmail.com>
2025-10-27 09:57:11 +0530

    update something

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+/**
+ * IMPORTANT for GitHub Pages:
+ * - CRA injects process.env.PUBLIC_URL from your "homepage" in package.json.
+ * - We pass it as the basename so routes become:
+ *   /Well-Vantage-Lead-Management/leads, etc.
+ */
+const container = document.getElementById("root");
+const root = createRoot(container);
+
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
